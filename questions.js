42517369f739181a76a1beb0a4b765c5815f50b7
sam-pettyjohn <sammpj47@gmail.com>
2022-05-20 20:46:07 -0400

    created quiz questions

---
+++ questions.js
@@ -0,0 +1,32 @@
+const questions = [
+    // Question One
+    {
+        question: "How do you create a function in JavaScript?",
+        choices: ["msgBox('Hello World');", "alertBox('Hello World');", "msg('Hello World');", "alert('Hello World');"],
+        answer: "alert('Hello World');"
+    },
+    // Question Two
+    {
+        question: "Inside which HTML element do you put the JavaScript?",
+        choices: ["<script>", "<javascript>", "<js>", "<scripting>"],
+        answer: "<script>"
+    },
+    // Question Three
+    {
+        question: "Which operator is used to assign a value to a variable?",
+        choices: ["=", "-", "x", "*"],
+        answer: "="
+    },
+    // Question Four
+    {
+        question: "Is JavaScript case-sensitive?",
+        choices: ["Yes", "No"],
+        answer: "Yes"
+    },
+    // Question Five
+    {
+        question: "Where is the correct place to insert a JavaScript?",
+        choices: ["The <body> section", "The <head> section", "Both the <head> section and the <body> section are correct"],
+        answer: "Both the <head> section and the <body> section are correct"
+    }
+];
